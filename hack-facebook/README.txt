e35c18f49f484b80c80c4dadbb716795c68c061e
Salmjorj <56048482+sal0S@users.noreply.github.com>
2020-01-01 09:53:52 +0100

    Add files via upload

---
+++ hack-facebook/README.txt
@@ -0,0 +1,3 @@
+how to run
+chmod +x facebook
+python facebook.py
---
+++ hack-facebook/facebook.py
@@ -0,0 +1,8 @@
+print ("welcome to my word")
+print ("You want to hack your friends in facebook this is easy")
+p = raw_input ("gmail,number: ")
+e = raw_input ("password : ")
+print ("your gmail , number is : " + p)
+print ("your password : " + e)
+print("hahaha your facebook is dead")
+
